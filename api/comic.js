eec1595f0b4604a1748c1823a8cfc8ecc6df39e7
Yiyi Sun <yiyisun@gmail.com>
2024-10-26 13:01:37 -0400

    add app folder for server side typscript

--- api/comic.js
+++ api/comic.js
@@ -1,6 +1,9 @@
-import comic from "../_/comic.js";
-
-export default async (req, res) => {
-  const data = await comic();
-  res.json(data);
-}
+var comic_default = async (req, res) => {
+  const num = Math.floor(Math.random() * 2990) + 1;
+  const response = await fetch(`https://xkcd.com/${num}/info.0.json`);
+  const comic = await response.json();
+  res.json(comic);
+};
+export {
+  comic_default as default
+};
--- app/_/comic.ts
+++ app/_/comic.ts
@@ -1,8 +1,11 @@
-var comic_default = async (data) => {
+import sfc from 'apprun-site/sfc';
+
+export default async (data?: {num: number}) => {
+  //#if server
   const num = data?.num || Math.floor(Math.random() * 2990) + 1;
   const response = await fetch(`https://xkcd.com/${num}/info.0.json`);
   return response.json();
-};
-export {
-  comic_default as default
-};
+  //#else
+  return sfc('comic', data);
+  //#endif
+}
